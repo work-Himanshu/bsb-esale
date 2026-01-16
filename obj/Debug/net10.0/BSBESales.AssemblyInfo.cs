ed1defa2057f3319d01a2846aac8d3a2ae78aa2c
work-Himanshu <work.himanshuroy@gmail.com>
2026-01-16 13:04:45 +0530

    gitIgnoreimplemented

--- obj/Debug/net10.0/BSBESales.AssemblyInfo.cs
+++ obj/Debug/net10.0/BSBESales.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BSBESales")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cdbb22843d47570be4ed55610c0a3bcfccfa75b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f357abcabdd4149c3abee74b3b89964c075d2fae")]
 [assembly: System.Reflection.AssemblyProductAttribute("BSBESales")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BSBESales")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
